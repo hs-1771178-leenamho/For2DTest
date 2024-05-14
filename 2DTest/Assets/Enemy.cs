3650f4ce3fc5ac141fbde5b10470f3cfff53a13d
LeeNamho <115772665+hs-1771178-leenamho@users.noreply.github.com>
2024-05-14 10:18:35 +0900

    수정중

--- 2DTest/Assets/Enemy.cs
+++ 2DTest/Assets/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Enemy : Chacter
 {
+    public Transform target;
     public Transform bullet;
     public Transform pos;
     // Start is called before the first frame update
@@ -16,9 +17,14 @@ public class Enemy : Chacter
     {
         while (true)
         {
-            Transform b = Instantiate(bullet, pos.transform.position, transform.rotation);
+            float fireTime = Random.Range(1f, 2.5f);
+            Vector2 dir = pos.position - target.position;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+            pos.rotation = Quaternion.Euler(0, 0, angle + 180f);
+            Transform b = Instantiate(bullet, pos.transform.position, pos.transform.rotation);
             b.SetParent(null);
-            yield return new WaitForSeconds(2.5f);
+            yield return new WaitForSeconds(fireTime);
         }
     }
 }
